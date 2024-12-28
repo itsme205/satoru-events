f4cdaef80d033660b6d62b08eb1df55845a2714b
itsme205 <itsme20771@gmail.com>
2024-12-29 00:35:52 +0300

    cfg update

--- src/config/prod.config.ts
+++ src/config/prod.config.ts
@@ -1,3 +1,13 @@
+import { OverwriteResolvable } from "discord.js";
+
 export const BotConfig = {
-  
-}
+  guildId: "760744808498069515" as string,
+  categoryPosition: 0 as number | undefined,
+  eventBanRoleId: "1299375983253786714" as string,
+  categoryDefaultPermissions: [] as OverwriteResolvable[],
+  announceChannelId: "1294997066103128074" as string,
+  roleIds: {
+    eventerRoleId: ["1294996646513475625"] as string[],
+    eventAdminRoleId: ["1294996635629391946"] as string[],
+  },
+};
